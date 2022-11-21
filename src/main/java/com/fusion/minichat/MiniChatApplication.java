2c8b2a9632f72b21f9a45d5492694cf72941d72d
Maliksncf <abdelmalik-el-kaddous.bougherara@sncf.fr>
2022-11-21 14:55:18 +0100

    Initial commit

---
+++ src/main/java/com/fusion/minichat/MiniChatApplication.java
@@ -0,0 +1,13 @@
+package com.fusion.minichat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fusion/minichat/MiniChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fusion.minichat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
